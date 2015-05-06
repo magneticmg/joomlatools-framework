3b4901c99756f80dcd1e372ea303e499238a9d59
Steven Rombauts <steven@timble.net>
2015-05-06 12:24:56 +0100

    re #224 - Move the bootstrap logic into the KoowaAutoloader class

--- KoowaAutoloader.php
+++ KoowaAutoloader.php
@@ -1,17 +1,57 @@
 <?php
 class KoowaAutoloader
 {
+    /**
+     * Koowa Bootstrapping
+     *
+     * If KOOWA is defined assume it was already loaded and bootstrapped
+     */
+    public function bootstrap()
+    {
+        if (!defined('KOOWA'))
+        {
+            /**
+             * Joomla Configuration
+             */
+            require_once JPATH_CONFIGURATION . '/configuration.php';
+            $config = new JConfig;
+
+            /**
+             * Joomla Version
+             */
+            require_once JPATH_LIBRARIES . '/cms/version/version.php';
+            $version = new JVersion;
+
+            /**
+             * Framework Bootstrapping
+             */
+            require_once __DIR__.'/../nooku-framework/code/koowa.php';
+            Koowa::getInstance(array(
+                'debug'           => $config->debug,
+                'cache'           => false, //config->caching
+                'cache_namespace' => 'koowa-' . JPATH_BASE === JPATH_SITE ? 'site' : 'admin' . '-' . md5($config->secret),
+                'root_path'       => JPATH_ROOT,
+                'base_path'       => JPATH_BASE,
+                'vendor_path'     => false //Composer loader is already registered.
+            ));
+
+            /**
+             * Component Bootstrapping
+             */
+            KObjectManager::getInstance()->getObject('object.bootstrapper')
+                ->registerComponents(JPATH_LIBRARIES.'/koowa/components', 'koowa')
+                ->registerApplication('site', JPATH_SITE . '/components', JPATH_BASE === JPATH_SITE)
+                ->registerApplication('admin', JPATH_ADMINISTRATOR . '/components', JPATH_BASE == JPATH_ADMINISTRATOR)
+                ->bootstrap();
+        }
+    }
+
     public function registerAutoloader($autoloader)
     {
         if (!defined('KOOWA')) {
-            $this->loadFramework();
+            $this->_bootstrap();
         }
 
         $autoloader();
-    }
-
-    public function loadFramework()
-    {
-        require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'autoload.php';
     }
 }
--- autoload.php
+++ autoload.php
@@ -7,44 +7,4 @@
  * @link        https://github.com/nooku/nooku-framework for the canonical source repository
  */
 
-/**
- * Koowa Bootstrapping
- *
- * If KOOWA is defined assume it was already loaded and bootstrapped
- */
-if(!defined('KOOWA'))
-{
-    /**
-     * Joomla Configuration
-     */
-    require_once JPATH_CONFIGURATION . '/configuration.php';
-    $config = new JConfig;
-
-    /**
-     * Joomla Version
-     */
-    require_once JPATH_LIBRARIES . '/cms/version/version.php';
-    $version = new JVersion;
-
-    /**
-     * Framework Bootstrapping
-     */
-    require_once __DIR__.'/../nooku-framework/code/koowa.php';
-    Koowa::getInstance(array(
-        'debug'           => $config->debug,
-        'cache'           => false, //config->caching
-        'cache_namespace' => 'koowa-' . JPATH_BASE === JPATH_SITE ? 'site' : 'admin' . '-' . md5($config->secret),
-        'root_path'       => JPATH_ROOT,
-        'base_path'       => JPATH_BASE,
-        'vendor_path'     => false //Composer loader is already registered.
-    ));
-
-    /**
-     * Component Bootstrapping
-     */
-    KObjectManager::getInstance()->getObject('object.bootstrapper')
-        ->registerComponents(JPATH_LIBRARIES.'/koowa/components', 'koowa')
-        ->registerApplication('site', JPATH_SITE . '/components', JPATH_BASE === JPATH_SITE)
-        ->registerApplication('admin', JPATH_ADMINISTRATOR . '/components', JPATH_BASE == JPATH_ADMINISTRATOR)
-        ->bootstrap();
-}
+KoowaAutoloader::bootstrap();
