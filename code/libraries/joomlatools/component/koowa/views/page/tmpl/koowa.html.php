3bebee2b7847923cbff03f7760af6160a570d5c0
Robin Poort <robin@timble.net>
2016-08-11 14:22:10 +0200

    re #309 - RTL styling

--- code/libraries/joomlatools/component/koowa/views/page/tmpl/koowa.html.php
+++ code/libraries/joomlatools/component/koowa/views/page/tmpl/koowa.html.php
@@ -7,28 +7,30 @@
  * @link        https://github.com/nooku/nooku-framework for the canonical source repository
  */
 
-defined('KOOWA') or die; ?>
+defined('KOOWA') or die;
+
+$app             = JFactory::getApplication();
+$doc             = JFactory::getDocument();
+$language  = $doc->language;
+$direction = $doc->direction;
+
+?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php echo $language; ?>" dir="<?php echo $direction; ?>">
 <head>
     <base href="<?= url(); ?>" />
     <title><?= title() ?></title>
-
     <meta content="text/html; charset=utf-8" http-equiv="content-type"  />
     <meta content="chrome=1" http-equiv="X-UA-Compatible" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
     <ktml:title>
     <ktml:meta>
     <ktml:link>
     <ktml:style>
     <ktml:script>
-
 </head>
 <body>
-
     <ktml:messages>
     <ktml:content>
-
 </body>
 </html>
